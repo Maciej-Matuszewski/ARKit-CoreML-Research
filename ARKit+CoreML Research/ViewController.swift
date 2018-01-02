d890cdc081c93161b9d166d6499a4aa75702ee1c
Maciej Matuszewski <maciej.matuszewski@netguru.pl>
2018-01-02 15:18:22 +0100

    Initial Commit

---
+++ ARKit+CoreML Research/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKit+CoreML Research
+//
+//  Created by Maciej Matuszewski on 02.01.2018.
+//  Copyright © 2018 Maciej Matuszewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
